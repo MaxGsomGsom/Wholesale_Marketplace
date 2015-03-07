43b7d6740ba0daba947e33abbcc4e09b5aba6dd5
GsomGsom <GsomGsom@1baee301-0139-441a-b302-27d2aaaffe84>
2015-03-07 21:58:03 +0000

    много

--- Wholesale_Marketplace/Wholesale_Marketplace/Controllers/HomeController.cs
+++ Wholesale_Marketplace/Wholesale_Marketplace/Controllers/HomeController.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Wholesale_Marketplace.Models;
 
 namespace Wholesale_Marketplace.Controllers
 {
     public class HomeController : Controller
     {
+        WMDB db = new WMDB();
         public ActionResult Index()
         {
+            Helpers.UserCheck(db, ViewBag);
             return View();
         }
 
---
+++ Wholesale_Marketplace/Wholesale_Marketplace/Models/Annotations.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Wholesale_Marketplace.Models
+{
+    [MetadataTypeAttribute(typeof(UserMetadata))]
+    public partial class User { }
+
+    class UserMetadata
+    {
+        [Required]
+        //[RegularExpression("^[a-zA-Z][a-zA-Z0-9-_\\.]{5,10}$")]
+        public string Login { get; set; }
+
+        [Required]
+        //[Range(4, 10)]
+        //[RegularExpression("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\\s).*$")]
+        public string Password { get; set; }
+
+        [Required]
+        //[RegularExpression("^[-\\w.]+@([A-z0-9][-A-z0-9]+\\.)+[A-z]{2,4}$")]
+        public string Email { get; set; }
+    }
+
+
+
+
+    [MetadataTypeAttribute(typeof(BuyerMetadata))]
+    public partial class Buyer { }
+
+    class BuyerMetadata
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+    }
+}
